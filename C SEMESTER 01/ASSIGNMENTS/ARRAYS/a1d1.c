31c9a9de93db91e96287ecfdf891a4ec2a2e298d
JAMPANI KOMAL <jampanikomal2005@gmail.com>
2023-12-14 14:44:43 +0530

    UPDATED FILES AS ON 14 DECEMBER 02:44 PM.

---
+++ C SEMESTER 01/ASSIGNMENTS/ARRAYS/a1d1.c
@@ -0,0 +1,58 @@
+//Write programs to manipulate arrays (sorting, searching, insertion, deletion).
+//Write a c programm to delete an element from a user defined 1d array. Array size and elements are user defined.
+#include <stdio.h>
+int main()
+{
+    int s, p;
+    printf("Enter the size of 1d array: ");
+    scanf("%d", &s);
+    int i, a[s];
+    printf("\n");
+    printf("Enter the values of 1d array:\n"); 
+
+    for(i=0; i<s; i++)
+    {
+        printf("Enter the value of element %d: ", i);
+        scanf("%d", &a[i]);
+    }
+    printf("\n");
+    printf("Your 1d array is: \n");
+
+    for(i=0; i<s; i++)
+    {
+        printf("{");
+        printf("%d, ", a[i]);
+        printf("}");
+        
+
+    }
+
+    printf("\n");
+
+    printf("Enter the index of the value in 1d array to be deleted: ");
+    scanf("%d", &p);
+
+    while (p>s)   
+    {
+        printf("Enter correct index value.\n");
+        printf("Enter the index of the value in 1d array to be deleted: ");
+        scanf("%d", &p);
+
+    } 
+        for(i=p; i<s-1; i++)
+        {
+            a[i]=a[i+1];
+        }
+        
+
+        printf("Your new 1d array is: \n");
+
+        for(i=0; i<s-1; i++)
+        {
+            printf("{");
+            printf("%d, ", a[i]);
+            printf("}");
+        
+        }          
+
+}
---
+++ C SEMESTER 01/CPLAB/14dec/14deca.c
@@ -0,0 +1,21 @@
+//Write a c programm to display employee details (employee id, name and department using structures.)
+#include <stdio.h>
+struct ofinfo 
+{
+    int id;
+    char * name;
+    char * department;
+};
+
+int main()
+{
+    struct ofinfo e1={01, "Tejas Prakash Patil", "R&D"};
+    struct ofinfo e2;
+    e2.id = 02;
+    e2.name = "Aashutosh Thakur";
+    e2.department = "Accounts";
+    printf("Emplyee 01:- id: %d, Name: %s, Department: %s.\n", e1.id, e1.name, e1.department);
+    printf("Emplyee 02:- id: %d, Name: %s, Department: %s.\n", e2.id, e2.name, e2.department);
+
+    return 0;
+}
