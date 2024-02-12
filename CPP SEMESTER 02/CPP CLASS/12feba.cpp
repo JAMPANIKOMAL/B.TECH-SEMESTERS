11a3ce3e74d6d3044f65a7c3959a478906a2b04a
JAMPANI <jampanikomal2005@gmail.com>
2024-02-12 14:07:13 +0530

    UPDATED FILES AS ON 12-02-2024 14:06 PM IN SITAICS GFL 05.

---
+++ CPP SEMESTER 02/CPP CLASS/12feba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Name: Jampani Komal\n";
+    cout << "City: Gandhinagar" << endl;
+    cout << "Country: India" << endl;
+    return 0;
+}
+// << insertion operator
+// >> extraction operator
+//The difference is endl imeediate flush to the output while \n do not.
---
+++ CPP SEMESTER 02/CPP CLASS/12febb.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number1, number2;
+    cout << "Enter First number: ";
+    cin >> number1;
+    cout << "Enter Second number: ";
+    cin >> number2;
+    cout << "Sum: " << number1+number2;
+
+    return 0;
+}
