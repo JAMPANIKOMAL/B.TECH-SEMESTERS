a1411a38cc807f189f151051adb6b8660f112a32
JAMPANI <jampanikomal2005@gmail.com>
2024-02-16 12:39:04 +0530

    UPDATED FILES AS ON 16-02-2024 12:38 PM IN SITAICS GFL 05.

---
+++ CPP SEMESTER 02/CPP CLASS/16feb/16feba.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int m=20;
+int main ()
+{
+    // :: Scope resolution operator.
+    int m=10;
+    cout << m << endl;
+    cout << ::m;
+
+    return 0;
+}
