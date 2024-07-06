b9d6462d1e9c4f1a266b263b762954c82962763c
JAMPANI <jampanikomal2005@gmail.com>
2024-07-06 19:04:34 +0530

    UFAO 06-07-2024 19:04 PM IN HOME IN HPVLT.

---
+++ SEMESTER 03/JAVA/OTHERS/july06/july06a.java
@@ -0,0 +1,10 @@
+class july06a
+{
+    public static void main(String a[])
+    {
+        System.out.println("Hello World!");
+        long lon = 3456l;
+        System.out.println(lon);
+        float f = 4.5f;
+    }
+}
