cd3f561c884be762e71f140364791dd297d14076
Jampani <jampanikomal2005@gmail.com>
2025-01-07 16:50:13 +0530

    .

--- SEMESTER 04/IC/LAB/jan/07jan/07janb.py
+++ SEMESTER 04/IC/LAB/jan/07jan/07janb.py
@@ -5,6 +5,6 @@ mt = ""
 
 for key in range(26):
     for x in ct:
-        mt = mt+chr(abs(ord(x) - (key - 1)%26))
+        mt = mt+chr(abs(ord(x) - (key)%26))
     print("Message text if key = ", key, ": ", mt)
     mt = ""
