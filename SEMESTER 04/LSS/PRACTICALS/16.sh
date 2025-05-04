8483d555f9e78ecdfaba4c0e297e76ad24a77af2
JAMPANIKOMAL <jampanikomal2005@gmail.com>
2025-05-04 23:15:32 +0530

    update

--- SEMESTER 04/LSS/PRACTICALS/16.sh
+++ SEMESTER 04/LSS/PRACTICALS/16.sh
@@ -1,3 +1,13 @@
+# === CRON SETUP INSTRUCTIONS ===
+# 1. Save this file as
+# 2. Make it executable:
+#      chmod +x 16.sh
+# 3. Edit your crontab:
+#      crontab -e
+# 4. Add the following line to run daily at 2:00 AM:
+#      0 2 * * * path_to 16.sh
+# ------------------------------------------
+
 #!/bin/bash
 
 backup_directory()
