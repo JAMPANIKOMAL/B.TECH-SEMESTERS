5c5ce74e799e2e6ef534b6ee3c9aaf5721a56239
JAMPANIKOMAL <jampanikomal2005@gmail.com>
2025-05-04 22:41:56 +0530

    update

--- SEMESTER 04/LSS/PRACTICALS/08.sh
+++ SEMESTER 04/LSS/PRACTICALS/08.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-child_process()
-{
-    echo "Parent PID: $$"
-    child_pid=$( (echo "Child PID: $$") & wait $! )
-}
+echo "Parent PID: $$"
 
-child_process
+# Define and run child code in a subshell using heredoc
+bash << 'END_CHILD' &
+echo "Child PID: $$"
+echo "Child's Parent PID: $PPID"
+END_CHILD
+
+# Wait for child to finish
+wait
