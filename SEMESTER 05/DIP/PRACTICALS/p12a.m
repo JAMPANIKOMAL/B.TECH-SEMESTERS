8208d20d2d7508acd7b8fd3df409e548b5d21bad
Jampani Komal <jampanikomal2005@gmail.com>
2025-11-06 10:42:30 +0530

    Create Practical 12

---
+++ SEMESTER 05/DIP/PRACTICALS/p12a.m
@@ -0,0 +1,28 @@
+clc;
+clear;
+close all;
+
+img = imread('sample/cameraman.jpeg');
+if size(img, 3) == 3
+    img = rgb2gray(img);
+end
+
+I_noisy = imnoise(img, 'salt & pepper', 0.05);
+
+w = [1 2 1; 2 4 2; 1 2 1];
+w = w / sum(w(:));
+
+I_smooth = imfilter(I_noisy, w, 'replicate');
+
+figure;
+subplot(1, 3, 1);
+imshow(img);
+title('Original Image');
+
+subplot(1, 3, 2);
+imshow(I_noisy);
+title('Salt & Pepper Noise');
+
+subplot(1, 3, 3);
+imshow(I_smooth);
+title('Weighted Average Smoothing');
